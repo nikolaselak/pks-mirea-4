5d84296f093a3065c2073dea4c67733159d899fc
fullstakilla <selakovich@mail.ru>
2024-04-18 17:02:55 +0300

    123

---
+++ 4praktika/Models/MyDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace _4praktika.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSnakeCaseNamingConvention();
+        }
+    }
+}
